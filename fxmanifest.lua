cf40fd56a047ba4ac17aaf1e0f25b1fc550c9226
MJGaming <35645498+mjgaming@users.noreply.github.com>
2021-08-27 20:31:31 -0500

    Update Manifest

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'InfiniteRPGTA'
+description 'Example resource'
+version '1.0.0'
+
+-- What to run
+client_scripts {
+    'client.lua'
+}
+server_script 'server.lua'
